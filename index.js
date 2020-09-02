b123ca9f27ecaafc154510facf5b0b27d804f31e
ellie fard <enesarifard@gmail.com>
2020-09-02 09:33:44 -0400

    added styling

--- index.js
+++ index.js
@@ -5,13 +5,13 @@ const authRouter = require('./routes/admin/auth');
 
 const app = express();
 
+app.use(express.static('public'));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(
   cookieSession({
     keys: ['lkasld235j'],
   })
 );
-
 app.use(authRouter);
 
 app.listen(3000, () => {
